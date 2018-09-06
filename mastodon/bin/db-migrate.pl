0a4fa5dc6f751ba07b433e138e431cb30538d81f
Johannes Ernst <jernst@indiecomputing.com>
2018-09-06 21:26:52 +0000

    Implement workaround for missing database permissions per https://github.com/uboslinux/ubos-mastodon/issues/7 Track upstream

--- mastodon/bin/db-migrate.pl
+++ mastodon/bin/db-migrate.pl
@@ -1,16 +1,35 @@
 #!/usr/bin/perl
 #
-# Initialize freshly installed Mastodon. Only invoke from ubos-manifest.json.
+# Initialize freshly installed Mastodon, or upgrade an existing one.
+# Only invoke from ubos-manifest.json.
 #
 
 use strict;
 
+use UBOS::Databases::PostgreSqlDriver;
 use UBOS::Logging;
 use UBOS::Utils;
 
-if( 'install' eq $operation ) {
+if( 'install' eq $operation || 'upgrade' eq $operation ) {
     my $appConfigId  = $config->getResolveOrNull( 'appconfig.appconfigid' );
     my $dataDir      = $config->getResolveOrNull( 'appconfig.datadir' );
+
+    my $dbUser       = $config->getResolveOrNull( 'appconfig.postgresql.dbuser.maindb' );
+    my $dbName       = $config->getResolveOrNull( 'appconfig.postgresql.dbname.maindb' );
+
+    # This seems to be a hack, but after restore from backup, our
+    # postgres user does not have any privileges any more to access
+    # this database, see https://github.com/uboslinux/ubos-mastodon/issues/7
+
+    unless( UBOS::Databases::PostgreSqlDriver::executeCmdAsAdmin(
+            'psql -v HISTFILE=/dev/null ' . $dbName,
+            <<SQL )) {
+GRANT ALL ON ALL TABLES IN SCHEMA public TO "$dbUser";
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO "$dbUser";
+GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO "$dbUser";
+SQL
+        error( 'Updating postgres user permissions failed' );
+    }
 
     my $cmd = 'cd ' . $dataDir . '/mastodon'
             . ' &&'
@@ -23,6 +42,7 @@ if( 'install' eq $operation ) {
     if( UBOS::Utils::myexec( $cmd, undef, \$out, \$out )) {
         error( "Rails db:migrate failed: cmd: $cmd, out: $out" );
     }
+
 }
 
 1;
